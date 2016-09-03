90c0840023a2590d61d60e5dc7cd380bccbc6396
Marshall Rose <mrose17@gmail.com>
2016-09-02 20:23:53 -0700

    fast patch to help eyeshade sychronization

--- src/controllers/wallet.js
+++ src/controllers/wallet.js
@@ -33,6 +33,8 @@ v1.read =
              }
     if (balanceP || refreshP) {
       balances = await runtime.wallet.balances(wallet)
+// temporary to help eyeshade synchronization
+      await runtime.queue.send(debug, 'wallet-report', underscore.extend({ paymentId: paymentId }, state.$set))
 
       if (!underscore.isEqual(balances, wallet.balances)) {
         state = { $currentDate: { timestamp: { $type: 'timestamp' } },
