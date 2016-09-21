92f485c3a1c1d00aee65c44c76e813e374deb2e2
Marshall Rose <mrose17@gmail.com>
2016-09-20 22:00:31 -0400

    0.8.73: use ledger-publisher@0.8.67

--- src/controllers/balance.js
+++ src/controllers/balance.js
@@ -2,10 +2,27 @@ var braveHapi = require('../brave-hapi')
 var bson = require('bson')
 var Joi = require('joi')
 var ledgerBalance = require('ledger-balance')
+var underscore = require('underscore')
 
 var v1 = {}
 
 var rulesetId = 2
+
+var rulesetEntry = async function (request, runtime) {
+  var entry
+  var debug = braveHapi.debug(module, request)
+  var version = runtime.npminfo.children['ledger-balance']
+  var rulesets = runtime.db.get('rulesets', debug)
+
+  entry = await rulesets.findOne({ rulesetId: rulesetId })
+  if ((!entry) || (entry.version.indexOf(version) !== 0)) {
+    if (entry) rulesets.remove({ rulesetId: rulesetId })
+
+    entry = { rules: ledgerBalance.providers, version: version }
+  }
+
+  return entry
+}
 
 /*
    GET /v1/balance/providers
@@ -14,7 +31,9 @@ var rulesetId = 2
 v1.read =
 { handler: function (runtime) {
   return async function (request, reply) {
-    reply(ledgerBalance.providers)
+    var entry = await rulesetEntry(request, runtime)
+
+    reply(entry.rules)
   }
 },
 
@@ -37,15 +56,15 @@ v1.create =
   return async function (request, reply) {
     var state
     var debug = braveHapi.debug(module, request)
+    var version = runtime.npminfo.children['ledger-balance'] + '-' + underscore.now()
     var rulesets = runtime.db.get('rulesets', debug)
 
     state = { $currentDate: { timestamp: { $type: 'timestamp' } },
-              $set: { rules: request.payload, type: 'balance/providers' }
+              $set: { rules: request.payload, version: version, type: 'balance/providers' }
             }
     await rulesets.update({ rulesetId: rulesetId }, state, { upsert: true })
-    ledgerBalance.providers = request.payload
 
-    reply({})
+    reply(version)
   }
 },
 
@@ -62,12 +81,69 @@ v1.create =
     { payload: ledgerBalance.schema },
 
   response:
-    { schema: Joi.object().length(0) }
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+-[1-9][0-9]+$/) }
+}
+
+/*
+   DELETE /v1/balance/providers
+ */
+
+v1.delete =
+{ handler: function (runtime) {
+  return async function (request, reply) {
+    var debug = braveHapi.debug(module, request)
+    var rulesets = runtime.db.get('rulesets', debug)
+
+    await rulesets.remove({ rulesetId: rulesetId })
+
+    reply(runtime.npminfo.children['ledger-balance'])
+  }
+},
+
+  auth:
+    { strategy: 'session',
+      scope: [ 'ledger' ],
+      mode: 'required'
+    },
+
+  description: 'Resets the list of ledger balance providers',
+  tags: [ 'api' ],
+
+  validate:
+    { query: {} },
+
+  response:
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+$/) }
+}
+
+/*
+   GET /v1/balance/providers/version
+ */
+
+v1.version =
+{ handler: function (runtime) {
+  return async function (request, reply) {
+    var entry = await rulesetEntry(request, runtime)
+
+    reply(entry.version)
+  }
+},
+
+  description: 'Returns the version of the ledger balance providers list',
+  tags: [ 'api' ],
+
+  validate:
+    { query: {} },
+
+  response:
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+(-[1-9]+[0-9]*)?$/) }
 }
 
 module.exports.routes = [
   braveHapi.routes.async().get().path('/v1/balance/providers').config(v1.read),
-  braveHapi.routes.async().post().path('/v1/balance/providers').config(v1.create)
+  braveHapi.routes.async().post().path('/v1/balance/providers').config(v1.create),
+  braveHapi.routes.async().delete().path('/v1/balance/providers').config(v1.delete),
+  braveHapi.routes.async().get().path('/v1/balance/providers/version').config(v1.version)
 ]
 
 module.exports.initialize = async function (debug, runtime) {
@@ -78,14 +154,13 @@ module.exports.initialize = async function (debug, runtime) {
   [ { category: rulesets,
       name: 'rulesets',
       property: 'rulesetId',
-      empty: { rulesetId: 0, type: '', timestamp: bson.Timestamp.ZERO },
+      empty: { rulesetId: 0, type: '', version: '', timestamp: bson.Timestamp.ZERO },
       unique: [ { rulesetId: 1 } ],
-      others: [ { type: 1 }, { timestamp: 1 } ]
+      others: [ { type: 1 }, { version: 1 }, { timestamp: 1 } ]
     }
   ])
 
   entry = await rulesets.findOne({ rulesetId: rulesetId })
-  if (entry) ledgerBalance.providers = entry.rules
-  validity = Joi.validate(ledgerBalance.providers, ledgerBalance.schema)
+  validity = Joi.validate(entry ? entry.rules : ledgerBalance.providers, ledgerBalance.schema)
   if (validity.error) throw new Error(validity.error)
 }
--- src/controllers/exchange.js
+++ src/controllers/exchange.js
@@ -2,12 +2,15 @@ var braveHapi = require('../brave-hapi')
 var bson = require('bson')
 var Joi = require('joi')
 var ledgerExchange // not a package, yet...
+var underscore = require('underscore')
 
 var v1 = {}
 
 var rulesetId = 3
 
 ledgerExchange = {
+  version: '0.0.0',
+
   providers: {
     AU: {
       exchangeName: 'CoinJar',
@@ -102,6 +105,22 @@ cubits.forEach((iso3166) => {
   ledgerExchange.providers[iso3166] = { exchangeName: 'Cubits', exchangeURL: 'https://cubits.com/' }
 })
 
+var rulesetEntry = async function (request, runtime) {
+  var entry
+  var debug = braveHapi.debug(module, request)
+  var version = ledgerExchange.version
+  var rulesets = runtime.db.get('rulesets', debug)
+
+  entry = await rulesets.findOne({ rulesetId: rulesetId })
+  if ((!entry) || (entry.version.indexOf(version) !== 0)) {
+    if (entry) rulesets.remove({ rulesetId: rulesetId })
+
+    entry = { rules: ledgerExchange.providers, version: version }
+  }
+
+  return entry
+}
+
 /*
    GET /v1/exchange/providers
  */
@@ -109,7 +128,9 @@ cubits.forEach((iso3166) => {
 v1.read =
 { handler: function (runtime) {
   return async function (request, reply) {
-    reply(ledgerExchange.providers)
+    var entry = await rulesetEntry(request, runtime)
+
+    reply(entry.rules)
   }
 },
 
@@ -132,15 +153,15 @@ v1.create =
   return async function (request, reply) {
     var state
     var debug = braveHapi.debug(module, request)
+    var version = ledgerExchange.version + '-' + underscore.now()
     var rulesets = runtime.db.get('rulesets', debug)
 
     state = { $currentDate: { timestamp: { $type: 'timestamp' } },
-              $set: { rules: request.payload, type: 'exchange/providers' }
+              $set: { rules: request.payload, version: version, type: 'balance/providers' }
             }
     await rulesets.update({ rulesetId: rulesetId }, state, { upsert: true })
-    ledgerExchange.providers = request.payload
 
-    reply({})
+    reply(version)
   }
 },
 
@@ -157,12 +178,69 @@ v1.create =
     { payload: ledgerExchange.schema },
 
   response:
-    { schema: Joi.object().length(0) }
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+-[1-9][0-9]+$/) }
+}
+
+/*
+   DELETE /v1/exchange/providers
+ */
+
+v1.delete =
+{ handler: function (runtime) {
+  return async function (request, reply) {
+    var debug = braveHapi.debug(module, request)
+    var rulesets = runtime.db.get('rulesets', debug)
+
+    await rulesets.remove({ rulesetId: rulesetId })
+
+    reply(ledgerExchange.version)
+  }
+},
+
+  auth:
+    { strategy: 'session',
+      scope: [ 'ledger' ],
+      mode: 'required'
+    },
+
+  description: 'Resets the list of ledger exchange providers',
+  tags: [ 'api' ],
+
+  validate:
+    { query: {} },
+
+  response:
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+$/) }
+}
+
+/*
+   GET /v1/exchange/providers/version
+ */
+
+v1.version =
+{ handler: function (runtime) {
+  return async function (request, reply) {
+    var entry = await rulesetEntry(request, runtime)
+
+    reply(entry.version)
+  }
+},
+
+  description: 'Returns the version of the ledger exchange providers list',
+  tags: [ 'api' ],
+
+  validate:
+    { query: {} },
+
+  response:
+    { schema: Joi.string().regex(/^[0-9]+\.[0-9]+\.[0-9]+(-[1-9]+[0-9]*)?$/) }
 }
 
 module.exports.routes = [
   braveHapi.routes.async().get().path('/v1/exchange/providers').config(v1.read),
-  braveHapi.routes.async().post().path('/v1/exchange/providers').config(v1.create)
+  braveHapi.routes.async().post().path('/v1/exchange/providers').config(v1.create),
+  braveHapi.routes.async().delete().path('/v1/exchange/providers').config(v1.delete),
+  braveHapi.routes.async().get().path('/v1/exchange/providers/version').config(v1.version)
 ]
 
 module.exports.initialize = async function (debug, runtime) {
@@ -173,14 +251,13 @@ module.exports.initialize = async function (debug, runtime) {
   [ { category: rulesets,
       name: 'rulesets',
       property: 'rulesetId',
-      empty: { rulesetId: 0, type: '', timestamp: bson.Timestamp.ZERO },
+      empty: { rulesetId: 0, type: '', version: '', timestamp: bson.Timestamp.ZERO },
       unique: [ { rulesetId: 1 } ],
-      others: [ { type: 1 }, { timestamp: 1 } ]
+      others: [ { type: 1 }, { version: 1 }, { timestamp: 1 } ]
     }
   ])
 
   entry = await rulesets.findOne({ rulesetId: rulesetId })
-  if (entry) ledgerExchange.providers = entry.rules
-  validity = Joi.validate(ledgerExchange.providers, ledgerExchange.schema)
+  validity = Joi.validate(entry ? entry.rules : ledgerExchange.providers, ledgerExchange.schema)
   if (validity.error) throw new Error(validity.error)
 }
