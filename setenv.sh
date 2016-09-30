0beb89123d136f2a57e2c2099109c7ecc9877d23
Willy <willy@entropi.info>
2016-09-30 01:17:52 +0000

    Configure GitHub authentication via ENV vars
    - GitHub authentication vars can be configured in setenv.sh:
     GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, IRON_KEYPASS, AND GITHUB_FORCE_HTTPS
    - GitHub authentication can be disabled for testing, controlled by GITHUB_DISABLE_AUTHENTICATION variable
      if not disabled, shell script throws error telling user to setup GitHub vars in setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,17 @@
+# BITGO configuration
 #export BITGO_TOKEN= # ADD TOKEN FROM test.bitgo.com / bitgo.com
 export BITGO_ENVIRONMENT=test
 #export BITGO_ENTERPRISE_ID= # ADD ENTERPRISE ID from test.bitgo.com / bitgo.com
 export BITGO_ESCROW_ADDRESS=""
 export BITGO_UNSPENDABLE_XPUB=xpub661MyMwAqRbcFE3JRXiBw2NCCREosgDdz6GfBTySy2sGhjK4Prd3WMW5LgqFoZC6Pv3vNRbvscyvEgcSDCoqpWELPGBDQr9ZYrGrrhtn3ge # REPLACE OR USE THIS ONE
+
+# Github Authentication config
+GITHUB_DISABLE_AUTHENTICATION=true
+#export GITHUB_CLIENT_ID= 
+#export GITHUB_CLIENT_SECRET= 
+#export IRON_KEYPASS= 
+#export GITHUB_FORCE_HTTPS= 
+
 export MONGODB_URI="mongo/test"
 export REDIS_URL="redis:6379"
 export NODE_ENV="development"
